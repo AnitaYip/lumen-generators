ebee9e4994f06402bdf862dfe6872a3972d04a2f
ayip <anita.yip@realpage.com>
2017-01-16 15:53:12 -0500

    updated as per the latest framework changes

--- src/Commands/ResourceCommand.php
+++ src/Commands/ResourceCommand.php
@@ -6,15 +6,14 @@ class ResourceCommand extends BaseCommand {
     protected $signature = 'rp:resource
         {name : Name of the resource.}';
 
-    protected $description = 'Generates a model, controller, repository interface,
-    repository, schema (json), validator and routes for RESTful resource';
+    protected $description = 'Generates a model, controller, repository,
+     schema (json-api), validator and routes for RESTful resource';
 
     protected $fields;
 
     private $commands = [
         'rp:model',
         'rp:controller',
-        'rp:repointf',
         'rp:repo',
         'rp:schema',
         'rp:validator',
